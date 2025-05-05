96850e8ff1f7d2f3f5c7e58a8c17f4f534e07680
Andreas Thorén <andreasthoren79@gmail.com>
2025-05-05 21:14:32 +0200

    Added DimensionABC to be used by AssemblerABC and BuilderABC

--- helpers/abstracts.py
+++ helpers/abstracts.py
@@ -4,44 +4,20 @@ from enum import Enum
 import cadquery as cq
 
 
-class BuilderABC(ABC):
-    @abstractmethod
-    def build_part(self, part_type: Enum) -> cq.Workplane:
-        """Build a part based on the given part type."""
-
-
-class AssemblerABC(ABC):
-    PartTypeEnum: type[Enum]
-    BuilderClass: type[BuilderABC]
+class DimensionMixin:
 
     def __init__(
         self,
         width: int | float,
         depth: int | float,
         height: int | float,
-        material_thickness: int | float | dict[str, int | float]
+        material_thickness: int | float | dict[str, int | float],
     ):
-        """
-        Initialize base assembler dimensions and builder.
-
-        Args:
-            width (int | float): Total width of the assembly.
-            depth (int | float): Total depth of the assembly.
-            height (int | float): Total height of the assembly.
-            material_thickness (int | float | dict[str, int | float]):
-                Thickness of the material used, or a mapping of material names
-                to thicknesses.
-        """
+        """Initialize base dimensions."""
         self._x_length = width
         self._y_length = depth
         self._z_length = height
         self._material_thickness = material_thickness
-        self._builder = self.BuilderClass()
-
-    @property
-    @abstractmethod
-    def metadata_map(self) -> dict[Enum, dict]:
-        pass
 
     @property
     def width(self):
@@ -57,7 +33,75 @@ class AssemblerABC(ABC):
 
     @property
     def material_thickness(self):
+        if isinstance(self._material_thickness, dict):
+            return self._material_thickness.copy()
         return self._material_thickness
+
+    def get_part_thickness(self, part_type: Enum) -> int | float:
+        """Get the thickness of a specific part."""
+        if isinstance(self._material_thickness, dict):
+            try:
+                return self._material_thickness[part_type]
+            except KeyError as exc:
+                raise ValueError(
+                    f"Material thickness for {part_type} not found in mapping:"
+                    f"\n{self._material_thickness}"
+                ) from exc
+        return self._material_thickness
+
+
+class BuilderABC(DimensionMixin, ABC):
+    @abstractmethod
+    def build_part(self, part_type: Enum) -> cq.Workplane:
+        """Build a part based on the given part type."""
+
+
+class AssemblerABC(DimensionMixin, ABC):
+    """
+    To be used as a base class for all assemblers.
+    Subclasses must implement the following:
+
+    1. Define the following class attributes:
+    - PartTypeEnum: An Enum class containing all part types.
+    - BuilderClass: A concrete Builder class that inherits from BuilderABC.
+
+    2. If additional instance variables are needed, implement a custom __init__ method.
+       The custom __init__ must call super().__init__() with width, depth, height
+       and material_thickness.
+
+    3. Implement the metadata_map property.
+       It must return a dictionary mapping part types (PartTypeEnum members)
+       to metadata dictionaries containing kwargs for the cq.Assembly.add method.
+    """
+
+    PartTypeEnum: type[Enum]
+    BuilderClass: type[BuilderABC]
+
+    def __init__(
+        self,
+        width: int | float,
+        depth: int | float,
+        height: int | float,
+        material_thickness: int | float | dict[str, int | float],
+    ):
+        """
+        Initialize base assembler dimensions and builder.
+
+        Args:
+            width (int | float): Total width of the assembly.
+            depth (int | float): Total depth of the assembly.
+            height (int | float): Total height of the assembly.
+            material_thickness (int | float | dict[str, int | float]):
+                Thickness of the material used, or a mapping of material names
+                to thicknesses.
+        """
+        super().__init__(width, depth, height, material_thickness)
+        self._builder = self.BuilderClass()
+
+    @property
+    @abstractmethod
+    def metadata_map(self) -> dict[Enum, dict]:
+        pass
 
     @property
     def builder(self) -> BuilderABC:
@@ -89,9 +133,7 @@ class AssemblerABC(ABC):
             assembly_data.append((cq_workplane, metadata))
         return assembly_data
 
-    def assemble(
-        self, assembly_parts: Iterable[Enum] | None = None
-    ) -> cq.Assembly:
+    def assemble(self, assembly_parts: Iterable[Enum] | None = None) -> cq.Assembly:
         assembly_parts = assembly_parts or tuple(self.PartTypeEnum)
         assembly = cq.Assembly()
         for part, metadata in self._get_assembly_data(assembly_parts):
