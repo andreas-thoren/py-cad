f599640abda59e81c70c45221f8d4375ae3a7f98
Andreas Thorén <andreasthoren79@gmail.com>
2025-05-27 10:34:24 +0200

    more

--- show_objects.py
+++ show_objects.py
@@ -1,4 +1,4 @@
-def show_garbage_sort_box():
+def get_garbage_sort_box():
     from projects.garbage_sort_box.project_data import DIMENSION_DATA
     from projects.garbage_sort_box.assembly import Assembler, Part
 
@@ -6,10 +6,10 @@ def show_garbage_sort_box():
     assembly_parts = [Part.BOTTOM, Part.LONG_SIDE, Part.SHORT_SIDE]
     # assembly = assembler.assemble(assembly_parts)
     assembly = assembler.assemble()
-    show_object(assembly, name="Garbage Sort Box Assembly")  # type: ignore
+    return assembly, "Garbage Sort Box Assembly"
 
 
-def show_plywood_box():
+def get_plywood_box():
     from primitives.plywood_box.project_data import Part
     from primitives.plywood_box.assembly import Assembler
     from projects.generic_plywood_box import BOX_DIMENSIONS
@@ -20,20 +20,20 @@ def show_plywood_box():
     assembly = Assembler.get_assembly(
         BOX_DIMENSIONS, visual_offset=visual_offset, assembly_parts=part_types
     )
-    show_object(assembly, name="Generic box")  # type: ignore
+    return assembly, "Generic box"
 
 
-def show_plywood_box_part():
+def get_plywood_box_part():
     from primitives.plywood_box.project_data import PartType
     from primitives.plywood_box.parts import Builder
     from projects.generic_plywood_box import BOX_DIMENSIONS
 
     part_type = PartType.LONG_SIDE_PANEL
     part = Builder.get_part(BOX_DIMENSIONS, part_type)
-    show_object(part, name=part_type.name.replace("_", " ").capitalize())
+    return part, part_type.name.replace("_", " ").capitalize()
 
 
-def show_basic_box():
+def get_basic_box():
     from primitives.basic_box.project_data import Part
     from primitives.basic_box.assembly import Assembler
     from projects.generic_box import BOX_DIMENSIONS
@@ -45,20 +45,20 @@ def show_basic_box():
     assembly = Assembler.get_assembly(
         BOX_DIMENSIONS, visual_offset=visual_offset, assembly_parts=part_types
     )
-    show_object(assembly, name="Basic box")  # type: ignore
+    return assembly, "Basic box"
 
 
-def show_basic_box_part():
+def get_basic_box_part():
     from primitives.basic_box.project_data import PartType
     from primitives.basic_box.parts import Builder
     from projects.generic_box import BOX_DIMENSIONS
 
     part_type = PartType.TOP
     part = Builder.get_part(BOX_DIMENSIONS, part_type)
-    show_object(part, name=part_type.name.replace("_", " ").capitalize())
+    return part, part_type.name.replace("_", " ").capitalize()
 
 
-def show_test_project():
+def get_test_project():
     from tests.test_project.project_data import DIMENSION_DATA, Part
     from tests.test_project.assembly import PartialAssemblerLeaf
 
@@ -66,12 +66,14 @@ def show_test_project():
     assembly_parts = [Part.BOTTOM, Part.LONG_SIDE, Part.SHORT_SIDE]
     # assembly = assembler.assemble(assembly_parts)
     assembly = assembler.assemble()
-    show_object(assembly, name="Garbage Sort Box Assembly")  # type: ignore
+    return assembly, "Garbage Sort Box Assembly"
 
 
-# show_garbage_sort_box()
-# show_test_project()
-# show_plywood_box_part()
-# show_plywood_box()
-show_basic_box()
-# show_basic_box_part()
+# assembly, name = get_garbage_sort_box()
+# assembly, name = get_test_project()
+# assembly, name = get_plywood_box_part()
+# assembly, name = get_plywood_box()
+assembly, name = get_basic_box()
+# assembly, name = get_basic_box_part()
+
+show_object(assembly, name)
